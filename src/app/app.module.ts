49479f477b8ea35aec92421a4603fa197cc61933
Josh Sommer <joshdsommer@gmail.com>
2021-11-16 11:11:44 -0500

    removing NGRX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { RouterModule } from '@angular/router';
-import { TodoListComponent } from './todo-list/todo-list.component';
-import { appRoutes } from './app.routes';
-import { TodoListService } from './todo-list/todo-list.service';
-import { TodoListPresentationComponent } from './todo-list/todo-list-presentation/todo-list-presentation.component';
-import { HttpClientModule } from '@angular/common/http';
-import { TodoFormComponent } from './shared/todo-form/todo-form.component';
-import { UpdateTodoComponent } from './update-todo/update-todo.component';
-import { TodoFormModule } from './shared/todo-form/todo-form.module';
-import { StoreModule } from '@ngrx/store';
-import { todosReducer } from './state/todo.reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { appRoutes } from "./app.routes";
+import { TodoFormModule } from "./shared/todo-form/todo-form.module";
+import { TodoListPresentationComponent } from "./todo-list/todo-list-presentation/todo-list-presentation.component";
+import { TodoListComponent } from "./todo-list/todo-list.component";
 
 @NgModule({
   imports: [
@@ -25,19 +17,12 @@ import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
     RouterModule.forRoot(appRoutes),
     ReactiveFormsModule,
     TodoFormModule,
-    StoreModule.forRoot({ todo: todosReducer, router: routerReducer }),
-    StoreDevtoolsModule.instrument({
-      maxAge: 35,
-      logOnly: false
-    }),
-    StoreRouterConnectingModule.forRoot()
   ],
   declarations: [
     AppComponent,
-    HelloComponent,
     TodoListComponent,
-    TodoListPresentationComponent
+    TodoListPresentationComponent,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/update-todo/update-todo.component.ts
+++ src/app/update-todo/update-todo.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
-import { getSelectTodo } from "../state/todos.selectors";
+import { map, switchMap } from "rxjs/operators";
+import { TodoListService } from "../todo-list/todo-list.service";
 
 @Component({
   selector: "app-update-todo",
@@ -12,16 +12,18 @@ import { getSelectTodo } from "../state/todos.selectors";
 export class UpdateTodoComponent implements OnInit {
   todo$: Observable<any>;
 
-  constructor(private activatedRoute: ActivatedRoute, private store: Store) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private todoService: TodoListService
+  ) {}
 
   ngOnInit() {
-    this.todo$ = this.store.select(getSelectTodo);
-    // this.todo$ = this.activatedRoute.params.pipe(
-    //   map(route => route['todoId']),
-    //   switchMap(todoId => this.todoListService.getByID(todoId))
-    // );
+    this.todo$ = this.activatedRoute.params.pipe(
+      map((route) => route["todoId"]),
+      switchMap((todoId) => this.todoService.getByID(todoId))
+    );
   }
   updateTodo(todo) {
-    // this.todoListService.update(todo).subscribe();
+    this.todoService.update(todo).subscribe();
   }
 }
