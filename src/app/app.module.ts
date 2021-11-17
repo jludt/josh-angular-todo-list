7ff6eaf9be411cf81c823c603814fd679715e170
JLudt <jludt11@gmail.com>
2021-11-17 11:13:34 -0500

    adding ngrx and a reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { AppComponent } from "./app.component";
 import { appRoutes } from "./app.routes";
 import { TodoFormModule } from "./shared/todo-form/todo-form.module";
+import { todosReducer } from "./todo-list/state/todos.reducers";
 import { TodoListPresentationComponent } from "./todo-list/todo-list-presentation/todo-list-presentation.component";
 import { TodoListComponent } from "./todo-list/todo-list.component";
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   imports: [
@@ -17,6 +21,18 @@ import { TodoListComponent } from "./todo-list/todo-list.component";
     RouterModule.forRoot(appRoutes),
     ReactiveFormsModule,
     TodoFormModule,
+    StoreModule.forRoot([]),
+    StoreModule.forFeature("todos", todosReducer),
+    StoreDevtoolsModule.instrument({
+      maxAge: 35,
+      logOnly: false,
+      features: {
+        pause: false,
+        lock: true,
+        persist: true,
+      },
+    }),
+    EffectsModule.forRoot([]),
   ],
   declarations: [
     AppComponent,
